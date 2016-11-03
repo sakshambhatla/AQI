f6f65e308ef178f49706882b8045ab684b93f89d
sakshambhatla <saksham.bhatla@gmail.com>
2016-11-03 15:57:01 -0400

    more cleaning up

--- AQI.py
+++ AQI.py
@@ -4,16 +4,19 @@ import urllib, re
 r = urllib.urlopen('http://airnow.gov/index.cfm?action=airnow.local_city&cityid=114').read()
 soup = BeautifulSoup(r, "html.parser")
 
-#Find the Air Quality Forecast table to the right.
-try:
-    letters = soup.find_all("table", class_="AQData")
-    #Narrow down to the Table's summary AQI (Today's High and Tomorrow's High)
-    temp2 = letters[2].find_all("tr")
-    #Get table showing Today's AQI
-    temp3 = temp2[2].find("table")
-    myText= temp3.getText().encode('utf-8').strip()
-except AttributeError as e:
-    print "Error parsing airgov.now. Website layout may have changed."
-    print e
-else:
-    print myText
+def parseAirGov():
+    #Find the Air Quality Forecast table to the right.
+    try:
+        rightTable = soup.find_all("table", class_="AQData")
+        #Narrow down to the Table's summary AQI (Today's High and Tomorrow's High)
+        subTable = rightTable[2].find_all("tr")
+        #Get table showing Today's AQI
+        todayTable = subTable[0].find("table")
+        myText= todayTable.getText().encode('utf-8').strip()
+    except AttributeError as e:
+        print "Error parsing airgov.now. Website layout may have changed.\n" , e
+    else:
+        return myText
+
+myText = parseAirGov()
+print myText
