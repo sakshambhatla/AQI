97f2edfaa7054396a57bfde7a7d976168533e2d7
sakshambhatla <saksham.bhatla@gmail.com>
2016-11-03 16:27:49 -0400

    more cleaning up

--- AQI.py
+++ AQI.py
@@ -1,22 +1,36 @@
 from bs4 import BeautifulSoup
 from IPython.display import HTML
 import urllib, re
+import string
+
 r = urllib.urlopen('http://airnow.gov/index.cfm?action=airnow.local_city&cityid=114').read()
 soup = BeautifulSoup(r, "html.parser")
 
+'''
+Parse the AirGov website
+Return AQI as list
+aqiList[0] - AQI value
+aqiList[1] - AQI text
+aqiList[2] - Additional info
+'''
 def parseAirGov():
-    #Find the Air Quality Forecast table to the right.
     try:
+        #Find the Air Quality Forecast table to the right.
         rightTable = soup.find_all("table", class_="AQData")
         #Narrow down to the Table's summary AQI (Today's High and Tomorrow's High)
         subTable = rightTable[2].find_all("tr")
         #Get table showing Today's AQI
-        todayTable = subTable[0].find("table")
+        todayTable = subTable[2].find("table")
         myText= todayTable.getText().encode('utf-8').strip()
     except AttributeError as e:
         print "Error parsing airgov.now. Website layout may have changed.\n" , e
     else:
-        return myText
+        #Cleanup text
+        aqiList = [s for s in string.split(myText, '\n') if s.strip()]
+        del aqiList[0]
+        regObj=re.match(r'.*(\d{2,3}).*', aqiList[0])
+        aqiList[0] = regObj.group(1)
+        return aqiList
 
-myText = parseAirGov()
-print myText
+myList = parseAirGov()
+print myList
