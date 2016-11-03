0053032f47cddfae2237f906c5dedb2740d28859
sakshambhatla <saksham.bhatla@gmail.com>
2016-11-03 15:38:33 -0400

    basic web scraping works

--- AQI.py
+++ AQI.py
@@ -3,19 +3,26 @@ from IPython.display import HTML
 import urllib, re
 r = urllib.urlopen('http://airnow.gov/index.cfm?action=airnow.local_city&cityid=114').read()
 soup = BeautifulSoup(r, "html.parser")
-print type(soup)
+#print type(soup)
 #print soup.prettify()[41900:42800]
 #letters = soup.find_all("table", class_="AQData")
 #regExp = re.compile('AIQ')
 #letters = soup.find_all("td", attrs={'class' : 'AQDataSectionTitle'}, text=regExp)
-letters = soup.find_all("td", class_="AQDataSectionTitle")
-temp=letters[2].find_all("td", attrs={'background':None})
-print letters
-'''ans = temp[7].find_all("td", attrs={'align':'center'})
-for line in ans:
-    print str(line.getText()).strip()
-    #print [int(s) for s in str(line.getText()).strip() if s.is()]
-    print "........."'''
+#Find the Air Quality Forecast table to the right.
+letters = soup.find_all("table", class_="AQData")
+#print letters[2]
+#Narrow down to the Table's summary AQI (Today's High and Tomorrow's High)
+temp2 = letters[2].find_all("tr")
+#print temp2[2]
+#Get table showing Today's AQI
+temp3 = temp2[2].find("table")
+#print temp3
+myText= temp3.getText().encode('utf-8').strip()
+print myText
+#letters[3] has
+#print letters
+
+
 #print str(ans[7])
 #matchObj = re.match( '.*([0-9][0-9]).*', str(ans[7]), re.I)
 #print matchObj[1]
